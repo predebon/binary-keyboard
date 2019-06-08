515f943c76ead5e0095dc242421a1bdfa58b41c7
Predebon <jv.predebon@hotmail.com>
2019-06-08 00:33:26 -0300

    Version 1.0 - Working as US layout keyboard

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # binary-keyboard
-STM32F0 based binary keyboard
+## An STM32F0 based binary keyboard
+Hi, so I've decided to make a keyboard with just 2 buttons on in to fool the new guys at my office, so me and my boss went head first to this project. It uses a ST microcontroller with native USB support and the exterior (excluding the keys) was 3D printed.
+
+![bin keyboard](https://raw.githubusercontent.com/predebon/binary-keyboard/master/bin_kbrd.png)
+
+### Hardware and Microcontroller
+I'm using my STM32F042F6 capibara 2 mini dev kit, having 2 buttons, 3 LEDs and a switch for the bootloader, on top of that, i've soldered 3 buttons to a perf board with the same size of my board, making it the 0, 1 and backspace buttons. The pull ups are internal.
+
+The pinout is as follows:
+>PA1 - Button '0'
+>PA2 - Button '1'
+>PA3 - Button 'Backspace'
+>PB8 - Used to enter on DFU mode / when booted, used to enter in debug mode
+
+On this project, the LED resistors were removed to avoid blinking lights inside of the case.
+
+### Using the Keyboard
+It is plug and play, just make sure your keyboard is configured to standard US layout.
+When you type groupings of 8 bits, the keyboard interprets it as a byte and writes the respective ASCII value.
+
+### How it Works
+When you type, the buffer is filled MSB first, so that you can read easily chars from the ASCII table. When the buffer is full, it is converted to the HID Report equivalent to that ASCII 
+
+Feel free to use this code for all sorts of STM32F0 applications, the function _easterEgg()_ can be used to write automatically any phrase you want.
+
+
---
+++ Src/main.c
@@ -0,0 +1,332 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_hid.h"
+extern USBD_HandleTypeDef hUsbDeviceFS;
+#include "hid_defines.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+
+/* USER CODE BEGIN PV */
+uint8_t keyboardReport[8]={0};
+uint8_t bitIndex=0;
+uint8_t binVal=0;
+uint8_t bot0_prev=0,bot1_prev=0,backspace_prev=0;
+uint8_t printBits=0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+/* USER CODE BEGIN PFP */
+void scanButtons();
+void resetValue();
+void appendBit(uint8_t bit);
+void sendChar(uint8_t c);
+void easterEgg();
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  scanButtons();
+	  HAL_Delay(15);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 0;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin : PB8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : zero_Pin one_Pin backspace_Pin */
+  GPIO_InitStruct.Pin = zero_Pin|one_Pin|backspace_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void scanButtons(){
+	uint8_t bot0=0,bot1=0,backspace=0;
+	printBits = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_8);
+	bot0_prev = bot0;
+	bot0 = !HAL_GPIO_ReadPin(zero_GPIO_Port, zero_Pin);
+	bot1_prev = bot1;
+	bot1 = !HAL_GPIO_ReadPin(one_GPIO_Port, one_Pin);
+	backspace_prev = backspace;
+	backspace = !HAL_GPIO_ReadPin(backspace_GPIO_Port, backspace_Pin);
+	HAL_Delay(1);
+	if (bot0 && bot1 && backspace && !bot0_prev && !bot1_prev && !backspace_prev)
+		easterEgg();
+	else if (bot0 && !bot0_prev){
+		appendBit(0);
+		while(bot0){
+			bot0 = !HAL_GPIO_ReadPin(zero_GPIO_Port, zero_Pin);
+			HAL_Delay(1);
+		}
+	}
+	else if (bot1 && !bot1_prev){
+		appendBit(1);
+		while(bot1){
+			bot1 = !HAL_GPIO_ReadPin(one_GPIO_Port, one_Pin);
+			HAL_Delay(1);
+		}
+	}
+	else if (backspace && !backspace_prev)
+		resetValue();
+	else
+		return;
+}
+void resetValue(){
+	if (printBits){
+		for (int i=0; i<bitIndex; i++)
+			sendChar(0x7F);
+	}
+	bitIndex=0;
+	binVal=0;
+}
+void appendBit(uint8_t bit){
+	binVal |= (bit<<(7-bitIndex));
+	if(printBits)
+		sendChar(bit+0x30);
+	bitIndex++;
+	if (bitIndex==8){
+		sendChar(binVal);
+		resetValue();
+	}
+}
+void sendChar(uint8_t c){
+	keyboardReport[0]=modifierTable[c];
+	keyboardReport[2]=HIDTable[c];
+	USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t*)keyboardReport, sizeof(keyboardReport));
+	HAL_Delay(20);
+	keyboardReport[0]=0;
+	keyboardReport[2]=0;
+	USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t*)keyboardReport, sizeof(keyboardReport));
+	HAL_Delay(20);
+}
+void easterEgg(){
+	  char frasedodiabo[]="Hail satan!\n";
+	  for (int i=0; i<strlen(frasedodiabo); i++)
+		    sendChar((uint8_t)frasedodiabo[i]);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
